47455a61ccc8c8f9287f7667ff003e4146c779a0
buccaneer33 <buccaneer33@inbox.ru>
2019-03-25 07:36:09 +0400

    vol wrong

---
+++ backend/www/index.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name=Buccaneer content=Buccaneer>
+<title>PHP</title>
+
+<body>
+	<main>
+		<section>
+			<div>
+			 <?php echo '<p>Hello World!</p>';?>
+			</div>
+		</section>
+
+		<section ></section>
+	</main>
+	<footer></footer>
+</html>
