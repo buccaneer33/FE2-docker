88c42aa4363c64d93b239e041754beb4db1403c5
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-04-02 11:22:23 +0400

    devel start

--- backend/scripts/100_run_apt-get_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get upgrade -y && docker-php-ext-install pdo pdo_mysql mysqli && a2enmod rewrite
--- backend/trash/200_run_apt-get_upgrade.sh
+++
@@ -1,3 +0,0 @@
-apt-get upgrade -y
-
-#bash /home/user_1/script/100_run_apt-get_update.sh
